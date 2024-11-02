6ad48254a0066bc7c3de6fe5421646c8fecb83c8
Victor Guillo <vicgu889@student.liu.se>
2024-11-02 17:06:29 +0100

    Modified the vignette and libraries in app

--- R/shiny/app.R
+++ R/shiny/app.R
@@ -1,6 +1,16 @@
 # app.R
 
 library(shiny)
+library(httr)
+library(jsonlite)
+library(R6)
+library(sf)
+library(dplyr)
+library( ggplot2)
+library(plotly)
+library(here)
+library(DT)
+
 
 # Source the UI and server code
 source("ui.R")
